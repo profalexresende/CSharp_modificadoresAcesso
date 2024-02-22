35d312aa51cfc29fea8dec2a6cd1f1d558b3a586
logonpflocal <logonpflocal@L1603MICRO100>
2024-02-22 09:12:14 -0300

    Add project files.

---
+++ modificadoresAcesso/ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modificadoresAcesso
+{
+    internal class ContaBancaria
+    {
+        // Exemplo de membros com diferentes modificadores de acesso
+        public int NumeroConta { get; set; }
+        private double Saldo { get; set; }
+        protected string Titular { get; set; }
+        internal bool Ativa { get; set; }
+
+        public ContaBancaria(int numeroConta, double saldoInicial, string titular)
+        {
+            NumeroConta = numeroConta;
+            Saldo = saldoInicial;
+            Titular = titular;
+            Ativa = true;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+            Console.WriteLine($"Depósito de {valor} realizado. Novo saldo: {Saldo}");
+        }
+
+        private void Sacar(double valor)
+        {
+            if (valor <= Saldo)
+            {
+                Saldo -= valor;
+                Console.WriteLine($"Saque de {valor} realizado. Novo saldo: {Saldo}");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para saque.");
+            }
+        }
+
+        protected void ExibirSaldo()
+        {
+            Console.WriteLine($"Saldo da conta {NumeroConta}: {Saldo}");
+        }
+
+    }
+}
---
+++ modificadoresAcesso/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using modificadoresAcesso;
+
+ContaBancaria minhaConta = new ContaBancaria(1234, 1000, "Alex");
+
+bool continuar = true;
+
+while (continuar)
+{
+    Console.WriteLine("\nMenu:");
+    Console.WriteLine("1. Exibir Saldo");
+    Console.WriteLine("2. Depositar");
+    Console.WriteLine("3. Sacar");
+    Console.WriteLine("4. Sair");
+
+    Console.Write("Escolha uma opção: ");
+    string opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            //minhaConta.ExibirSaldo(); ERRO: exibirSaldo não é acessível
+            break;
+
+        case "2":
+            Console.Write("Digite o valor para depósito: ");
+            double valorDeposito = Convert.ToDouble(Console.ReadLine());
+            minhaConta.Depositar(valorDeposito);
+            break;
+
+        case "3":
+            Console.Write("Digite o valor para saque: ");
+            double valorSaque = Convert.ToDouble(Console.ReadLine());
+            //minhaConta.Sacar(valorSaque); ERRO o sacar não é acessível
+            break;
+
+        case "4":
+            continuar = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            break;
+    }
+}
+    
